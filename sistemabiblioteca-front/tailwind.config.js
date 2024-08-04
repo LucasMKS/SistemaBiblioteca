bbed5e8142fa992157f17ee16b45e9cdb8731336
LucasMKS <lucas.mar17ques@gmail.com>
2024-08-04 14:59:12 -0300

    Adicionado variaveis de cores

--- sistemabiblioteca-front/tailwind.config.js
+++ sistemabiblioteca-front/tailwind.config.js
@@ -59,6 +59,106 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        primarymi: {
+          DEFAULT: "hsl(var(--primary-mi))",
+          foreground: "hsl(var(--onPrimary-mi))",
+        },
+        primaryContainer: {
+          DEFAULT: "hsl(var(--primaryContainer-mi))",
+          foreground: "hsl(var(--onPrimaryContainer-mi))",
+        },
+        secondarmiy: {
+          DEFAULT: "hsl(var(--secondary-mi))",
+          foreground: "hsl(var(--onSecondary-mi))",
+        },
+        secondaryContainer: {
+          DEFAULT: "hsl(var(--secondaryContainer-mi))",
+          foreground: "hsl(var(--onSecondaryContainer-mi))",
+        },
+        tertiary: {
+          DEFAULT: "hsl(var(--tertiary-mi))",
+          foreground: "hsl(var(--onTertiary-mi))",
+        },
+        tertiaryContainer: {
+          DEFAULT: "hsl(var(--tertiaryContainer-mi))",
+          foreground: "hsl(var(--onTertiaryContainer-mi))",
+        },
+        error: {
+          DEFAULT: "hsl(var(--error-mi))",
+          foreground: "hsl(var(--onError-mi))",
+        },
+        errorContainer: {
+          DEFAULT: "hsl(var(--errorContainer-mi))",
+          foreground: "hsl(var(--onErrorContainer-mi))",
+        },
+        backgroundmi: {
+          DEFAULT: "hsl(var(--background-mi))",
+          foreground: "hsl(var(--onBackground-mi))",
+        },
+        surface: {
+          DEFAULT: "hsl(var(--surface-mi))",
+          foreground: "hsl(var(--onSurface-mi))",
+        },
+        surfaceVariant: {
+          DEFAULT: "hsl(var(--surfaceVariant-mi))",
+          foreground: "hsl(var(--onSurfaceVariant-mi))",
+        },
+        outline: {
+          DEFAULT: "hsl(var(--outline-mi))",
+          foreground: "hsl(var(--outlineVariant-mi))",
+        },
+        shadow: {
+          DEFAULT: "hsl(var(--shadow-mi))",
+          foreground: "hsl(var(--scrim-mi))",
+        },
+        inverseSurface: {
+          DEFAULT: "hsl(var(--inverseSurface-mi))",
+          foreground: "hsl(var(--inverseOnSurface-mi))",
+        },
+        inversePrimary: {
+          DEFAULT: "hsl(var(--inversePrimary-mi))",
+          foreground: "hsl(var(--foreground-mi))",
+        },
+        primaryFixed: {
+          DEFAULT: "hsl(var(--primaryFixed-mi))",
+          foreground: "hsl(var(--onPrimaryFixed-mi))",
+        },
+        primaryFixedDim: {
+          DEFAULT: "hsl(var(--primaryFixedDim-mi))",
+          foreground: "hsl(var(--onPrimaryFixedVariant-mi))",
+        },
+        secondaryFixed: {
+          DEFAULT: "hsl(var(--secondaryFixed-mi))",
+          foreground: "hsl(var(--onSecondaryFixed-mi))",
+        },
+        secondaryFixedDim: {
+          DEFAULT: "hsl(var(--secondaryFixedDim-mi))",
+          foreground: "hsl(var(--onSecondaryFixedVariant-mi))",
+        },
+        tertiaryFixed: {
+          DEFAULT: "hsl(var(--tertiaryFixed-mi))",
+          foreground: "hsl(var(--onTertiaryFixed-mi))",
+        },
+        tertiaryFixedDim: {
+          DEFAULT: "hsl(var(--tertiaryFixedDim-mi))",
+          foreground: "hsl(var(--onTertiaryFixedVariant-mi))",
+        },
+        surfaceDim: {
+          DEFAULT: "hsl(var(--surfaceDim-mi))",
+          foreground: "hsl(var(--surfaceBright-mi))",
+        },
+        surfaceContainerLowest: {
+          DEFAULT: "hsl(var(--surfaceContainerLowest-mi))",
+          foreground: "hsl(var(--surfaceContainerLow-mi))",
+        },
+        surfaceContainer: {
+          DEFAULT: "hsl(var(--surfaceContainer-mi))",
+          foreground: "hsl(var(--surfaceContainerHigh-mi))",
+        },
+        surfaceContainerHighest: {
+          DEFAULT: "hsl(var(--surfaceContainerHighest-mi))",
+          foreground: "hsl(var(--surfaceContainerHighest-mi))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
