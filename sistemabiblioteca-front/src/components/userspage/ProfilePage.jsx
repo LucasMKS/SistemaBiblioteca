b2316e967a723ac6963819e729a6c8af7f237913
LucasMKS <lucas.mar17ques@gmail.com>
2024-08-07 18:41:31 -0300

    Lista de emprestimos adicionado ao Perfil

--- sistemabiblioteca-front/src/components/userspage/ProfilePage.jsx
+++ sistemabiblioteca-front/src/components/userspage/ProfilePage.jsx
@@ -4,12 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from "../ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 export default function ProfilePage({ onLogout }) {
   const navigate = useNavigate();
-  const [profileInfo, setProfileInfo] = useState({});
+  const [profileInfo, setProfileInfo] = useState([]);
+  const [loanData, setLoanData] = useState([]);
   const [userData, setUserData] = useState({
     id: "",
     name: "",
@@ -17,10 +19,18 @@ export default function ProfilePage({ onLogout }) {
     matricula: "",
     role: "",
   });
+  const [bookData, setBookData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchProfileInfo();
   }, []);
+
+  useEffect(() => {
+    if (userData.matricula) {
+      fetchLoan(userData.matricula);
+    }
+  }, [userData.matricula]);
 
   const handleUpdateClick = (id) => {
     setUserData((prevUserData) => ({
@@ -45,6 +55,35 @@ export default function ProfilePage({ onLogout }) {
     }
   };
 
+  const fetchLoan = async (matricula) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await UserService.getLoanByMatricula(matricula, token);
+      const loans = response.ourLoanList || [];
+      setLoanData(loans);
+      fetchBookDataForLoans(loans);
+    } catch (error) {
+      console.error("Error fetching loans:", error);
+    }
+  };
+
+  const fetchBookDataForLoans = async (loans) => {
+    const token = localStorage.getItem("token");
+    const bookDataTemp = {};
+    for (const loan of loans) {
+      if (!bookDataTemp[loan.isbn]) {
+        try {
+          const response = await UserService.getBookByIsbn(loan.isbn, token);
+          bookDataTemp[loan.isbn] = response.book;
+        } catch (error) {
+          console.error("Error fetching book data for ISBN:", loan.isbn, error);
+        }
+      }
+    }
+    setBookData(bookDataTemp);
+    setIsLoading(false);
+  };
+
   const fetchProfileInfo = async () => {
     try {
       const token = localStorage.getItem("token");
@@ -62,10 +101,9 @@ export default function ProfilePage({ onLogout }) {
     try {
       const token = localStorage.getItem("token");
       const res = await UserService.userUpdateEmail(profileInfo.id, userData, token);
-      if (profileInfo.email != userData.email) {
+      if (profileInfo.email !== userData.email) {
         handleLogout();
       }
-      console.log(res);
       fetchProfileInfo();
     } catch (error) {
       console.error("Erro ao atualizar usuário:", error);
@@ -74,10 +112,9 @@ export default function ProfilePage({ onLogout }) {
   };
 
   return (
-    <main className="h-screen flex justify-center items-center">
-
-      <div className="w-full max-w-screen-md rounded-md overflow-hidden border">
-        <Card>
+    <main className="flex h-screen justify-center items-center bg-black">
+      <div className="flex flex-col items-center">
+        <Card className="bg-surfaceContainer-foreground">
           <CardHeader>
             <CardTitle className="text-center">Perfil</CardTitle>
             <CardDescription className="text-center">
@@ -86,24 +123,16 @@ export default function ProfilePage({ onLogout }) {
           </CardHeader>
           <CardContent className="grid grid-cols-2 items-center gap-4">
             <div className="flex items-center justify-between col-span-2">
-              <Label htmlFor="name" className="mb-2">
-                Nome: {profileInfo.name}
-              </Label>
+              <Label htmlFor="name" className="mb-2">Nome: {profileInfo.name}</Label>
             </div>
             <div className="flex items-center justify-between col-span-2">
-              <Label htmlFor="matricula" className="mb-2">
-                Matrícula: {profileInfo.matricula}
-              </Label>
+              <Label htmlFor="matricula" className="mb-2">Matrícula: {profileInfo.matricula}</Label>
             </div>
             <div className="flex items-center justify-between col-span-2">
-              <Label htmlFor="role" className="mb-2">
-                Email: {profileInfo.email}
-              </Label>
+              <Label htmlFor="role" className="mb-2">Email: {profileInfo.email}</Label>
             </div>
             <div className="flex items-center justify-between col-span-2">
-              <Label htmlFor="role" className="mb-2">
-                Função: {profileInfo.role}
-              </Label>
+              <Label htmlFor="role" className="mb-2">Função: {profileInfo.role}</Label>
               {/* Ação Alterar */}
               <Dialog>
                 <DialogTrigger asChild>
@@ -121,15 +150,11 @@ export default function ProfilePage({ onLogout }) {
                   <form onSubmit={updateUser}>
                     <div className="grid gap-4 py-4">
                       <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                          Nome
-                        </Label>
+                        <Label htmlFor="name" className="text-right">Nome</Label>
                         <Input id="name" name="name" value={userData.name} onChange={handleInputChange} className="col-span-3" />
                       </div>
                       <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="email" className="text-right">
-                          Email
-                        </Label>
+                        <Label htmlFor="email" className="text-right">Email</Label>
                         <Input id="email" name="email" value={userData.email} onChange={handleInputChange} className="col-span-3" />
                       </div>
                       <Label htmlFor="email" className="text-center text-xs">Alterar o email ira deslogar</Label>
@@ -145,6 +170,32 @@ export default function ProfilePage({ onLogout }) {
             </div>
           </CardContent>
         </Card>
+        <div className="flex flex-wrap gap-6 m-6">
+          {isLoading ? (
+            <Label className="flex text-center text-xl"><Loader2 className="mr-2 h-6 w-6 animate-spin" /> Carregando Emprestimos...</Label>
+          ) : (
+            Array.isArray(loanData) && loanData.map((loan, index) => (
+              <div key={index} className="flex flex-col border p-4 rounded w-72 bg-surface">
+                <div className="flex items-center space-x-2 mt-2 mb-2">
+                  <Label htmlFor="emprestimo" className="font-bold">Título: </Label>
+                  <Label htmlFor="emprestimo" className="">{bookData[loan.isbn]?.titulo}</Label>
+                </div>
+                <div className="flex items-center space-x-2 mt-2 mb-2">
+                  <Label htmlFor="emprestimo" className="font-bold">ISBN: </Label>
+                  <Label htmlFor="emprestimo" className="">{loan.isbn}</Label>
+                </div>
+                <div className="flex items-center space-x-2 mt-2 mb-2">
+                  <Label htmlFor="emprestimo" className="font-bold">Empréstimo: </Label>
+                  <Label htmlFor="emprestimo">{loan.dataEmprestimo}</Label>
+                </div>
+                <div className="flex items-center space-x-2 mt-2">
+                  <Label htmlFor="emprestimo" className="font-bold">Devolução: </Label>
+                  <Label htmlFor="emprestimo">{loan.dataDevolucao}</Label>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </main>
   );
